cc119d1fc49aacbc3b6a61441a31b314601f36d7
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-25 09:26:32 -0400

    added first interpolation

--- mapping/Grid.java
+++ mapping/Grid.java
@@ -1,11 +1,12 @@
 package mapping;
+import java.util.Random;
+
 import coordinates.*;
 
 public class Grid {
 
     static int tileCount = 10;
     static int tileSize = 5;
-
     int xActual, yActual;
 
     public Tile[] tiles = new Tile[tileCount*tileCount];
@@ -25,26 +26,33 @@ public class Grid {
         for (int x = 0; x < tileCount; x++) {
             for (int y = 0; y < tileCount; y++) {
 
-                float height = dotProduct(x,y);
-                tiles[id] = new Tile(x+xActual,y+yActual,height,tileSize);
+                // INTERPOLATION
+                float yFrac = (float)y/tileCount, xFrac = (float)x/tileCount;
+                float[] products = dotProduct(xFrac,yFrac);
+
+                float AB = products[0] + xFrac * (products[1]-products[0]);
+                float CD = products[2] + xFrac * (products[3]-products[2]);
+
+                float value = AB+yFrac*(CD-AB);
+
+                tiles[id] = new Tile(x+xActual,y+yActual,value*10,tileSize);
                 id++;
             }
         }
     }
 
-    private float dotProduct(int x, int y) {
+    private float[] dotProduct(float x, float y) {
         Vector2 vector = new Vector2(x, y);
-        float height = 0;
+        float[] products = new float[gradients.length];
 
         for (int g = 0; g < gradients.length; g++) {
             float xNew = (float)vector.x * gradients[g].x;
             float yNew = (float)vector.y * gradients[g].y;
 
             float sum = xNew+yNew;
-            height += sum;
+            products[g] = sum;
         }
-
-        return height;
+        return products;
     }
 
     float lerp(float min, float max, float value) {
@@ -57,6 +65,7 @@ public class Grid {
         int[] information = {
             tileSize, tileCount
         };
+
         return information;
     }
 }
